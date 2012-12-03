1d8e0bb0c8d3d94124dcf4ad2da917fde30e3a1f
James Arthur <thruflo@gmail.com>
2012-12-03 09:12:06 +0000

    setup: require pyramid 1.3 or higher.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     include_package_data = True,
     zip_safe = False,
     install_requires = [
-        'pyramid',
+        'pyramid>=1.3',
     ],
     tests_require = [
         'WebTest >= 1.3.1',
